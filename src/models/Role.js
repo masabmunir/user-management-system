f0b8b02e90e2f3a411f335fc0af74ee613ddb967
Masab Munir <masabk214@gmail.com>
2025-09-09 10:27:32 +0500

    userRole completed

--- src/models/Role.js
+++ src/models/Role.js
@@ -212,3 +212,186 @@ roleSchema.index({ status: 1 });
 roleSchema.index({ isDeleted: 1 });
 roleSchema.index({ department: 1 });
 roleSchema.index({ organization: 1 });
+
+// Virtual for all permissions (direct + inherited)
+
+roleSchema.virtual('allPermissions').get(function (){
+    const directPermissions = this.permissions || [];
+    const inheritedPermissions = this.inheritedPermissions || [];
+
+    // Combine and deduplicate permissions
+    const allPerms = [...directPermissions, ...inheritedPermissions];
+    return [...new Set(allPerms.map(p => p.toString()))];
+})
+
+// Pre-save middleware to handle role hierarchy
+roleSchema.pre('save', async function(next) {
+  // If parent role is set, add this role to parent's children
+  if (this.parentRole && this.isModified('parentRole')) {
+    await this.constructor.findByIdAndUpdate(
+      this.parentRole,
+      { $addToSet: { childRoles: this._id } }
+    );
+  }
+  
+  next();
+});
+
+// Pre-save middleware to calculate inherited permissions
+roleSchema.pre('save', async function(next) {
+  if (this.parentRole && (this.isModified('parentRole') || this.isModified('permissions'))) {
+    await this.calculateInheritedPermissions();
+  }
+  next();
+});
+
+// Instance method to calculate inherited permissions
+roleSchema.methods.calculateInheritedPermissions = async function() {
+  if (!this.parentRole) {
+    this.inheritedPermissions = [];
+    return;
+  }
+  
+  const parentRole = await this.constructor.findById(this.parentRole)
+    .populate('permissions')
+    .populate('inheritedPermissions');
+  
+  if (parentRole) {
+    // Inherit all permissions from parent (direct + inherited)
+    const parentAllPermissions = [
+      ...(parentRole.permissions || []),
+      ...(parentRole.inheritedPermissions || [])
+    ];
+    
+    // Remove duplicates
+    const uniquePermissions = [...new Set(parentAllPermissions.map(p => p._id.toString()))];
+    this.inheritedPermissions = uniquePermissions;
+  }
+};
+
+// Instance method to get all child roles recursively
+roleSchema.methods.getAllChildRoles = async function() {
+  const childRoles = [];
+  const queue = [this._id];
+  
+  while (queue.length > 0) {
+    const currentRoleId = queue.shift();
+    const currentRole = await this.constructor.findById(currentRoleId);
+    
+    if (currentRole && currentRole.childRoles) {
+      for (const childRoleId of currentRole.childRoles) {
+        if (!childRoles.includes(childRoleId.toString())) {
+          childRoles.push(childRoleId.toString());
+          queue.push(childRoleId);
+        }
+      }
+    }
+  }
+  
+  return childRoles;
+};
+
+// Instance method to check if role can be assigned to user
+roleSchema.methods.canAssignToUser = function() {
+  if (this.status !== 'active') return false;
+  if (this.maxUsers && this.currentUserCount >= this.maxUsers) return false;
+  return true;
+};
+
+// Instance method to add change to history
+roleSchema.methods.addToChangeHistory = function(action, changedBy, details, reason) {
+  this.changeHistory.push({
+    action,
+    changedBy,
+    changedAt: new Date(),
+    details,
+    reason
+  });
+};
+
+// Static method to find active roles
+roleSchema.statics.findActiveRoles = function() {
+    return this.find({
+        status: 'active',
+        isDeleted: false
+    });
+}   
+
+// Static method to find default role
+roleSchema.static.defaultRole = function (){
+    return this.findOne({
+        isDefault : 'true',
+        status: 'active',
+        isDeleted: false
+    })
+}
+
+// Static method to find roles by level
+roleSchema.static.defaultRole = function (level){
+    return this.find({
+        level,
+        status: 'active',
+        isDeleted: false
+    })
+}
+
+// Static method to build role hierarchy tree
+roleSchema.statics.buildHierarchyTree = async function() {
+  const roles = await this.find({ isDeleted: false })
+    .populate('permissions')
+    .populate('parentRole')
+    .populate('childRoles')
+    .sort({ level: 1 });
+  
+  const roleMap = new Map();
+  const rootRoles = [];
+  
+  // Create role map
+  roles.forEach(role => {
+    roleMap.set(role._id.toString(), {
+      ...role.toObject(),
+      children: []
+    });
+  });
+  
+  // Build hierarchy
+  roles.forEach(role => {
+    if (role.parentRole) {
+      const parent = roleMap.get(role.parentRole._id.toString());
+      if (parent) {
+        parent.children.push(roleMap.get(role._id.toString()));
+      }
+    } else {
+      rootRoles.push(roleMap.get(role._id.toString()));
+    }
+  });
+  
+  return rootRoles;
+};
+
+// Query middleware to exclude soft deleted documents
+roleSchema.pre(/^find/, function(next) {
+  if (!this.getQuery().isDeleted) {
+    this.find({ isDeleted: { $ne: true } });
+  }
+  next();
+});
+
+// Post middleware to update user count
+roleSchema.post('save', async function() {
+  if (this.isModified('permissions') || this.isNew) {
+    // Update user count for this role
+    const User = mongoose.model('User');
+    const userCount = await User.countDocuments({ 
+      roles: this._id, 
+      isDeleted: false 
+    });
+    
+    await this.constructor.findByIdAndUpdate(this._id, { 
+      currentUserCount: userCount 
+    });
+  }
+});
+
+const Role = mongoose.model('Role', roleSchema);
+module.exports = Role;
